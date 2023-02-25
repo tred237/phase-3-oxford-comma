e103f4803c3dd67558b2b95ac26e1e696c26c001
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-24 20:19:41 -0700

    commit tree 59318f7ca62a2fe2cb41121a8527c398174e0aa5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    last_index = array.length - 1
+    new_arr = []
+
+    array.each_with_index do |element, index|
+        if index != last_index || last_index == 0
+            new_arr.push(element)
+        else
+            new_arr.push("and #{element}")
+        end
+    end
+    last_index == 1 ? new_arr.join(" ") : new_arr.join(", ")
 end
+
+puts oxford_comma(['this', 'that', 'another'])
+puts oxford_comma(['this', "that"])
+puts oxford_comma(['this'])
